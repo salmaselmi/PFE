7f1e6262f15c89d8df1c689e694954205f8479a8
Salma98 <79660609+salmaselmi@users.noreply.github.com>
2021-05-18 18:52:24 +0100

    Add files via upload

---
+++ user.ts
@@ -0,0 +1,14 @@
+export class User {
+    constructor(
+    public nom: string,
+    public prenom: string,
+    public email: string,
+    public password: string,
+    public phone: string,
+    public date: string,
+    public fonctionnalite: string,
+    public _id?: string 
+
+    )
+     { }
+ }
