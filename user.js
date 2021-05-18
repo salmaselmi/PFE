74470ba4b52caa11dca010e67d5ac4d5cb96786e
Salma98 <79660609+salmaselmi@users.noreply.github.com>
2021-05-18 18:58:14 +0100

    USERMODAL(NODEJS)

---
+++ user.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.model('users',
+    {
+        nom: {
+            type: String,
+            required: true
+        },
+        prenom: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        phone: {
+            type: String
+        },
+        date:{
+            type:String
+        },
+        fonctionnalite: {
+            type: String,
+            required: true
+    
+        },
+       
+        role: {
+            type: String,
+            required: true,
+            default: 'user' 
+        }
+    })
+
+module.exports = User 
